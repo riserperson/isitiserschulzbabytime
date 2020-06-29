dd1b4fa704f575cf31275d300dd3cc41200afa50
rai <richard.iserman@gmail.com>
2020-06-29 18:37:42 -0400

    fixing

--- config.py
+++ config.py
@@ -5,10 +5,4 @@ from os import path
 BASE_DIRECTORY = path.abspath(path.dirname(__file__))
 DEBUG = False
 SECRET_KEY = "ozymandias"
-LANGUAGES = ['en', 'fr', 'ar', 'en_US', 'ar_TN', 'fr_FR']
 
-# Database details
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASE_DIRECTORY, 'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
